70934e9e5ece4a27307af2e292abc47d3b0a1b8d
David Jegat <david.jegat@gmail.com>
2026-02-23 11:34:23 +0100

    feat: add method and getter + setters php snips

--- etc/arcante-nvim/lua/snippets/php/php_class.lua
+++ etc/arcante-nvim/lua/snippets/php/php_class.lua
@@ -50,6 +50,76 @@ local php_attribute_snippet = s({ trig = "attr", desc = "PHP attribute snippet" 
 	t(";"),
 })
 
+local php_getter_snippet = s({ trig = "get", desc = "PHP getter method snippet" }, {
+	t("public function get"),
+	f(function(args)
+		local name = args[1][1]
+		return name:sub(1, 1):upper() .. name:sub(2)
+	end, { 1 }),
+	t("(): "),
+	i(2, "string"),
+	t({ "", "{", "\t" }),
+	t("return $this->"),
+	i(1, "name"),
+	t({ ";", "}" }),
+})
+
+local php_setter_snippet = s({ trig = "set", desc = "PHP setter method snippet" }, {
+	t("public function set"),
+	f(function(args)
+		local name = args[1][1]
+		return name:sub(1, 1):upper() .. name:sub(2)
+	end, { 1 }),
+	t("("),
+	i(2, "string"),
+	t(" $"),
+	i(1, "name"),
+	t({ "): static", "{", "\t$this->" }),
+	f(function(args)
+		return args[1][1]
+	end, { 1 }),
+	t(" = $"),
+	f(function(args)
+		return args[1][1]
+	end, { 1 }),
+	t({ ";", "", "\treturn $this;", "}" }),
+})
+
+local php_getset_snippet = s({ trig = "getset", desc = "PHP getter and setter snippet" }, {
+	t("public function get"),
+	f(function(args)
+		local name = args[1][1]
+		return name:sub(1, 1):upper() .. name:sub(2)
+	end, { 1 }),
+	t("(): "),
+	i(2, "string"),
+	t({ "", "{", "\treturn $this->" }),
+	i(1, "name"),
+	t({ ";", "}", "", "" }),
+	t("public function set"),
+	f(function(args)
+		local name = args[1][1]
+		return name:sub(1, 1):upper() .. name:sub(2)
+	end, { 1 }),
+	t("("),
+	f(function(args)
+		return args[1][1]
+	end, { 2 }),
+	t(" $"),
+	f(function(args)
+		return args[1][1]
+	end, { 1 }),
+	t({ "): static", "{", "\t$this->" }),
+	f(function(args)
+		return args[1][1]
+	end, { 1 }),
+	t(" = $"),
+	f(function(args)
+		return args[1][1]
+	end, { 1 }),
+	t({ ";", "", "\treturn $this;", "}" }),
+})
+
 local php_method_snippet = s({ trig = "fn", desc = "PHP method snippet" }, {
 	i(1, "public"),
 	t(" function "),
@@ -80,6 +150,9 @@ return {
 	php_class_snippet,
 	php_construct_snippet,
 	php_attribute_snippet,
+	php_getter_snippet,
+	php_setter_snippet,
+	php_getset_snippet,
 	php_method_snippet,
 	php_method_one_snippet,
 }
